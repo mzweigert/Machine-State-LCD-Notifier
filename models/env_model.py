c3bf64e10c181b45f30da394c5676baa9b27d926
mzweigert <mateuszzweigert@gmail.com>
2018-11-02 23:29:04 +0100

    Add last reboot datetime notification

--- models/env_model.py
+++ models/env_model.py
@@ -1,4 +1,5 @@
 class EnvironmentState:
-    def __init__(self, server_running=None, internet_on=None):
+    def __init__(self, server_running=None, internet_on=None, last_reboot_datetime=None):
         self.server_running = server_running
         self.internet_on = internet_on
+	self.last_reboot_datetime = last_reboot_datetime
--- run_notifier.py
+++ run_notifier.py
@@ -5,6 +5,7 @@ from models import env_model
 from drivers import lcddriver
 import schedule
 import time
+import os
 
 env_state = env_model.EnvironmentState()
 lcd = lcddriver.lcd()
@@ -21,7 +22,6 @@ def refresh_internet_connection_info():
         env_state.internet_on = internet_on
         internet_on_answer = convert_bool_to_string(internet_on)
         lcd.display_string("Internet on:     " + internet_on_answer, 1)
-        lcd.display_string("--------------------", 2)
 
 
 def refresh_server_info():
@@ -29,13 +29,26 @@ def refresh_server_info():
     if server_running != env_state.server_running:
         env_state.server_running = server_running
         server_running_answer = convert_bool_to_string(server_running)
-        lcd.display_string("Server running:  " + server_running_answer, 3)
-        lcd.display_string("--------------------", 4)
+        lcd.display_string("Server running:  " + server_running_answer, 2)
+
+
+def refresh_last_reboot_datetime():
+    cmd = os.popen("uptime --since")
+    last_reboot_datetime = cmd.read()
+    if last_reboot_datetime != env_state.last_reboot_datetime:
+	env_state.last_reboot_datetime = last_reboot_datetime
+	lcd.display_string("Last reboot datetime", 3)
+	date = last_reboot_datetime[:10]
+	time = last_reboot_datetime[11:19]
+	lcd.display_string(time + "  " + date, 4)
+
+    cmd.close()
 
 
 def refresh_lcd():
     refresh_internet_connection_info()
     refresh_server_info()
+    refresh_last_reboot_datetime()
 
 
 refresh_lcd()
